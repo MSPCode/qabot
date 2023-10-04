f1d0ac8ea855692b0c86171a70f684371ad746c1
MSPCode <45906899+MSPCode@users.noreply.github.com>
2023-10-04 09:13:51 -0400

    Reverted back to HTML content

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,23 +24,31 @@ def app():
         if user_input:
             response = generate_response(user_input)
             
-            # Response Section
+            # Adding Response section
             st.header("Response")
             for document in response:
-                st.text(document.metadata['title'])
-                st.write("---")  # Horizontal line as a separator
-
-            # Source(s) Section
+                st.markdown(f"""
+                    <div style='border:2px solid black; padding:10px; margin-bottom:10px; word-wrap: break-word;'>
+                        <strong>{document.metadata['title']}</strong>
+                    </div>
+                """, unsafe_allow_html=True)
+            
+            # Existing Source(s) section
             st.header("Source(s)")
             for document in response:
-                st.text(f"{document.metadata['title']} (First 25 words)")
-                st.text(document.page_content[:25])
-                if len(document.page_content) > 25:
-                    with st.expander("Show more"):
-                        st.text(document.page_content[25:])
-                st.text(f"**Source:** {document.metadata['source']}")
-                st.write("---")  # Horizontal line as a separator
-
+                st.markdown(f"""
+                    <div style='border:2px solid black; padding:10px; margin-bottom:10px; word-wrap: break-word;'>
+                        <strong>{document.metadata['title']}</strong> (First 25 words)
+                        <br>
+                        {document.page_content[:25]}
+                        <details>
+                            <summary>Show more</summary>
+                            {document.page_content[25:]}
+                        </details>
+                        <br>
+                        <strong>Source:</strong> {document.metadata['source']}
+                    </div>
+                """, unsafe_allow_html=True)
         else:
             st.error("Please enter your question.")
 
