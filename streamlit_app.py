206e2a6f3a3802872918713b0c5d3a1d14044366
MSPCode <45906899+MSPCode@users.noreply.github.com>
2023-10-04 00:24:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,27 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+def generate_response(question):
+    # Placeholder for the actual implementation of generate_response
+    # Return an empty list for now
+    return []
 
-st.write('Hello world!')
+def app():
+    st.title("Question Answering App")
+    st.markdown("How can I help you? Ask your question below.")
+
+    user_input = st.text_area("Your Question:")
+    if st.button("Submit"):
+        if user_input:
+            response = generate_response(user_input)
+            st.header("Source(s)")
+            for document in response:
+                with st.beta_expander(f"{document.metadata['title']} (First 25 words)"):
+                    st.write(document.page_content[:25])
+                    if len(document.page_content) > 25:
+                        st.write(document.page_content[25:])
+                    st.markdown(f"**Source:** {document.metadata['source']}")
+        else:
+            st.error("Please enter your question.")
+
+if __name__ == "__main__":
+    app()
