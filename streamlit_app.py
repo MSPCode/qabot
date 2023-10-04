9e5eb2fc4b6b184b7e4c064e6168f19b29e6d3d3
MSPCode <45906899+MSPCode@users.noreply.github.com>
2023-10-04 08:55:09 -0400

    Remove HTML

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-
 class Document:
     def __init__(self, page_content, metadata):
         self.page_content = page_content
@@ -10,8 +9,8 @@ def generate_response(question):
     # Placeholder for your generate_response function
     # Return a list of Document objects for demonstration
     documents = [
-        Document("This is the first document's contentqrqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwer.", {"source": "https://google.com", "title": "Title 1"}),
-        Document("This is the second document's content.contentqrqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwercontentqrqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwercontentqrqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwercontentqrqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwercontentqrqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwercontentqrqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwer", {"source": "google.com", "title": "Title 2"})
+        Document("This is the first document's content.", {"source": "https://google.com", "title": "Title 1"}),
+        Document("This is the second document's content.", {"source": "https://google.com", "title": "Title 2"})
     ]
     return documents
 
@@ -24,30 +23,24 @@ def app():
     if st.button("Submit"):
         if user_input:
             response = generate_response(user_input)
-            # Adding Response section
+            
+            # Response Section
             st.header("Response")
             for document in response:
-                st.markdown(f"""
-                    <div style='border:2px solid black; padding:10px; margin-bottom:10px; word-wrap: break-word;'>
-                        <strong>{document.metadata['title']}</strong>
-                    </div>
-                """, unsafe_allow_html=True)
-                
+                st.text(document.metadata['title'])
+                st.write("---")  # Horizontal line as a separator
+
+            # Source(s) Section
             st.header("Source(s)")
             for document in response:
-                st.markdown(f"""
-                    <div style='border:2px solid black; padding:10px; margin-bottom:10px; word-wrap: break-word;'>
-                        <strong>{document.metadata['title']}</strong> (First 25 words)
-                        <br>
-                        {document.page_content[:25]}
-                        <details>
-                            <summary>Show more</summary>
-                            {document.page_content[25:]}
-                        </details>
-                        <br>
-                        <strong>Source:</strong> {document.metadata['source']}
-                    </div>
-                """, unsafe_allow_html=True)
+                st.text(f"{document.metadata['title']} (First 25 words)")
+                st.text(document.page_content[:25])
+                if len(document.page_content) > 25:
+                    with st.expander("Show more"):
+                        st.text(document.page_content[25:])
+                st.text(f"**Source:** {document.metadata['source']}")
+                st.write("---")  # Horizontal line as a separator
+
         else:
             st.error("Please enter your question.")
 
