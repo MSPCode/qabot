a043b40ee037100291ea410c400d186c8ed4b070
MSPCode <45906899+MSPCode@users.noreply.github.com>
2023-10-04 00:29:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,22 @@
 import streamlit as st
 
+class Document:
+    def __init__(self, page_content, metadata):
+        self.page_content = page_content
+        self.metadata = metadata
+
 def generate_response(question):
-    # Placeholder for the actual implementation of generate_response
-    # Return an empty list for now
-    return []
+    # Placeholder for your generate_response function
+    # Return a list of Document objects for demonstration
+    documents = [
+        Document("This is the first document's content.", {"source": "Source 1", "title": "Title 1"}),
+        Document("This is the second document's content.", {"source": "Source 2", "title": "Title 2"})
+    ]
+    return documents
 
 def app():
     st.title("Question Answering App")
+
     st.markdown("How can I help you? Ask your question below.")
 
     user_input = st.text_area("Your Question:")
