f0a4f6a40c54e6521cf72d8b7d0b83918c227be2
MSPCode <45906899+MSPCode@users.noreply.github.com>
2023-10-06 17:45:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,78 @@
 import streamlit as st
-
-class Document:
-    def __init__(self, page_content, metadata):
-        self.page_content = page_content
-        self.metadata = metadata
+import os
+from langchain.vectorstores import Pinecone
+import pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
 
 def generate_response(question):
-    # Placeholder for your generate_response function
-    # Return a list of Document objects for demonstration
-    documents = [
-        Document("This is the first document's content.", {"source": "https://google.com", "title": "Title 1"}),
-        Document("This is the second document's content.", {"source": "https://google.com", "title": "Title 2"})
-    ]
-    return documents
+    text_field = "text"
+
+    pinecone.init(api_key=st.secrets["PINECONE_API_KEY"],environment='gcp-starter')
+    # switch back to normal index for langchain
+    index = pinecone.Index(st.secrets["INDEX_NAME"] )
+    os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+
+    vectorstore = Pinecone(index, OpenAIEmbeddings(), text_field)
+
+    #LLM setup
+    llm = OpenAI(temperature=0, model_name='gpt-3.5-turbo', openai_api_key=st.secrets["OPENAI_API_KEY"])
+
+    #previous model
+    qa_with_sources = RetrievalQAWithSourcesChain.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vectorstore.as_retriever()
+    )
+
+    #current model
+    prompt_template = """
+    Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you 
+    don't know or ask the user to provide more context. Do not ever try to make up an answer and always provide a source document. 
+    All of the questions asked here are only related to Veeva CRM administration, so always answer as if you’re talking to a Veeva CRM administrator.
+    {context}
+    Question: {question}
+    """
+
+    PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+
+    chain_type_kwargs = {"prompt": PROMPT}
+    
+    qa = RetrievalQA.from_chain_type(
+        llm=llm, 
+        chain_type="stuff", 
+        retriever=vectorstore.as_retriever(search_type="mmr", search_kwargs={'fetch_k': 30, 'k':4},chain_type_kwargs=chain_type_kwargs), 
+        return_source_documents=True)
+
+    result = qa({"query": question})
+
+    return result
 
 def app():
-    st.title("Question Answering App")
+    st.set_page_config(page_title="🤖🔗 AdminHelpep")
+    st.title('🤖🔗 AdminHelper')
 
     st.markdown("How can I help you? Ask your question below.")
 
-    user_input = st.text_area("Your Question:")
+    user_input = st.text_area("Question:")
     if st.button("Submit"):
         if user_input:
             response = generate_response(user_input)
             
             # Response Section
             st.header("Response")
-            for document in response:
-                st.text(document.metadata['title'])
-                st.write("---")  # Horizontal line as a separator
+            st.write(response['result'].strip())
+            st.write("---")  # Horizontal line as a separator
 
             # Source(s) Section
             st.header("Source(s)")
-            for document in response:
+            for document in response['source_documents']:
                 st.text(f"{document.metadata['title']}")
                 with st.expander("Show more"):
-                    st.text(document.page_content)
+                    st.write(document.page_content)
                 st.markdown(f"**Source:** {document.metadata['source']}")
                 st.write("---")  # Horizontal line as a separator
 
